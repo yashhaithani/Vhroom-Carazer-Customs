cfb8de73b745fc5b1558d9f26332420524d56720
YASH AITHANI <78597770+yashhaithani@users.noreply.github.com>
2024-05-29 22:29:06 +0530

    Update BuySellRent.js

--- BuySellRent/BuySellRent.js
+++ BuySellRent/BuySellRent.js
@@ -4,13 +4,20 @@ document.getElementById("buyBtn").addEventListener("click", function() {
 
 document.getElementById("sellBtn").addEventListener("click", function() {
     document.getElementById("sellForm").style.display = "block";
+    document.getElementById("rentForm").style.display = "none"; // Hide rent form if it's currently displayed
 });
 
 document.getElementById('rentBtn').addEventListener('click', function() {
     document.getElementById('rentForm').style.display = 'block';
+    document.getElementById('sellForm').style.display = 'none'; // Hide sell form if it's currently displayed
 });
 
 document.getElementById("sellFormFields").addEventListener("submit", function(event) {
+    event.preventDefault();
+    window.location.href = "confirmation.html";
+});
+
+document.getElementById("rentFormFields").addEventListener("submit", function(event) {
     event.preventDefault();
     window.location.href = "confirmation.html";
 });
